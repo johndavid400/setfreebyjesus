5f18676abbc6d0fe69555ec9577c2e6b52d8534e
johndavid400 <jd@isotope11.com>
2012-06-19 11:01:19 -0500

    hello setfree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Setfreebyjesus::Application.config.secret_token = 'a871cf140aad71d4dcbe80e41aeab326b9b7d496e31a0ecbf4974fb9de4089e04e35ac39e1f0ca1a6cd662d64e52db42b4ae44b7f17f4e69b8d2f796ade9e768'
