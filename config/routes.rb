d2736c559c334ae203b58a0b953430d0474fdcce
J.D. Warren <johndavid400@gmail.com>
2014-12-01 10:40:48 -0600

    update testimonies page

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   get "home/support"
   get "home/products"
   get "home/donations"
-  get "home/testimonials"
+  get "home/testimonies"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
